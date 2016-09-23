652ae3929d735d69299651263acec331fd629b2a
László ÁSHIN <laszlo@ashin.hu>
2016-09-23 07:25:58 +0200

    Improve program structure

--- tetris.cpp
+++ tetris.cpp
@@ -3,6 +3,9 @@
 #include <unistd.h>
 
 #include <algorithm>
+#include <memory>
+#include <iostream>
+#include <cassert>
 
 __attribute__((noreturn))
 static void
@@ -19,52 +22,152 @@ die(const char* file, int line, const char* expr)
 
 #define CHECK(expr) do { if (!(expr)) die(__FILE__, __LINE__, #expr); } while (0)
 
-struct Deadline {
-	unsigned remaining() const { return std::max(0u, value_ - SDL_GetTicks()); }
-	bool have_time() const { return value_ > SDL_GetTicks(); }
+struct Model {
+	void
+	update()
+	{
+		x_ += dx_;
+		if (x_ <= 0) {
+			x_ = 1;
+			dx_ = 5;
+		} else if (x_ >= 640) {
+			x_ = 640;
+			dx_ = -5;
+		}
+	}
+
+	int getX() const { return x_; }
 
 private:
-	uint32_t value_ = SDL_GetTicks() + 40;
+	int x_ = 0;
+	int dx_ = 5;
 };
+
+using SdlWindow = std::unique_ptr<SDL_Window, void(*)(SDL_Window*)>;
+
+struct Sdl {
+	Sdl() { CHECK(SDL_Init(SDL_INIT_VIDEO) >= 0); }
+	Sdl(const Sdl&) = delete;
+	Sdl& operator=(const Sdl&) = delete;
+	~Sdl() { SDL_Quit(); }
+
+	SdlWindow
+	createWindow()
+	{
+		SDL_Window* window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+		CHECK(window);
+		return SdlWindow(window, SDL_DestroyWindow);
+	}
+};
+
+struct SdlColor {
+	uint8_t r, g, b, opacity;
+
+	SdlColor(uint8_t red, uint8_t green, uint8_t blue, uint8_t opacity = SDL_ALPHA_OPAQUE)
+	: r(red), g(green), b(blue), opacity(opacity)
+	{}
+
+	static SdlColor black() { return SdlColor(0, 0, 0); }
+	static SdlColor red() { return SdlColor(255, 0, 0); }
+};
+
+struct SdlPoint {
+	int x, y;
+};
+
+struct SdlRenderer {
+	SdlRenderer(SdlWindow& window)
+	: rend_(SDL_CreateRenderer(window.get(), -1, SDL_RENDERER_ACCELERATED), SDL_DestroyRenderer)
+	{
+		if (!rend_) throw std::runtime_error("SDL_CreateRenderer");
+	}
+
+	void setDrawColor(SdlColor c) { SDL_SetRenderDrawColor(rend_.get(), c.r, c.g, c.b, c.opacity); }
+	void clear() { SDL_RenderClear(rend_.get()); }
+	void drawLine(SdlPoint a, SdlPoint b) { SDL_RenderDrawLine(rend_.get(), a.x, a.y, b.x, b.y); }
+	void present() { SDL_RenderPresent(rend_.get()); }
+
+private:
+	std::unique_ptr<SDL_Renderer, void(*)(SDL_Renderer*)> rend_;
+};
+
+struct SdlView {
+	SdlView(const Model& model, SdlRenderer& rend) : model_(model), rend_(rend) {}
+
+	void
+	render()
+	{
+		rend_.setDrawColor(SdlColor::black());
+		rend_.clear();
+		rend_.setDrawColor(SdlColor::red());
+		rend_.drawLine(SdlPoint{model_.getX(), 50}, SdlPoint{100, 100});
+		rend_.present();
+	}
+
+private:
+	const Model& model_;
+	SdlRenderer& rend_;
+};
+
+struct SdlEvents {
+	struct iterator {
+		iterator(SdlEvents* se) : se_(se) {}
+
+		bool operator!=(const iterator& rhs) const { return se_ != rhs.se_; }
+		iterator& operator++() { assert(se_); se_ = se_->next(); return *this; }
+		SDL_Event& operator*() { assert(se_); return se_->ev_; }
+
+	private:
+		SdlEvents* se_;
+	};
+
+	iterator begin() { return iterator(next()); }
+	iterator end() { return iterator(nullptr); }
+
+private:
+	struct Deadline {
+		unsigned remaining() const { return std::max(0u, value_ - SDL_GetTicks()); }
+		bool have_time() const { return value_ > SDL_GetTicks(); }
+
+	private:
+		uint32_t value_ = SDL_GetTicks() + 40;
+	};
+
+	bool hasNext() { return dl_.have_time() && SDL_WaitEventTimeout(&ev_, dl_.remaining()); }
+	SdlEvents* next() { return hasNext() ? this : nullptr; }
+
+	const Deadline dl_;
+	SDL_Event ev_;
+};
+
+static void
+requestSdlQuit()
+{
+	SDL_Event e;
+	e.type = SDL_QUIT;
+	e.quit.timestamp = SDL_GetTicks();
+	SDL_PushEvent(&e);
+}
 
 int
 main()
 {
-	CHECK(SDL_Init(SDL_INIT_VIDEO) >= 0);
-	SDL_Window *const window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-	CHECK(window);
-	SDL_Renderer *const rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	CHECK(rend);
-	bool quit = false;
-	int x = 0;
-	int dx = 5;
-	while (!quit) {
-		SDL_Event ev;
-		const Deadline dl;
-		while (dl.have_time() && SDL_WaitEventTimeout(&ev, dl.remaining())) {
+	Sdl sdl;
+	auto window = sdl.createWindow();
+	SdlRenderer rend(window);
+	Model model;
+	SdlView view(model, rend);
+	while (true) {
+		for (const auto& ev : SdlEvents()) {
 			if (ev.type == SDL_QUIT) {
-				quit = true;
+				return EXIT_SUCCESS;
 			} else if (ev.type == SDL_KEYDOWN) {
 				if (ev.key.keysym.sym == SDLK_ESCAPE) {
-					quit = true;
+					requestSdlQuit();
 				}
 			}
 		}
-		SDL_SetRenderDrawColor(rend, 0, 0, 0, SDL_ALPHA_OPAQUE);
-		SDL_RenderClear(rend);
-		SDL_SetRenderDrawColor(rend, 255, 0, 0, SDL_ALPHA_OPAQUE);
-		SDL_RenderDrawLine(rend, x, 50, 100, 100);
-		SDL_RenderPresent(rend);
-		x += dx;
-		if (x <= 0) {
-			x = 1;
-			dx = 5;
-		} else if (x >= 640) {
-			x = 640;
-			dx = -5;
-		}
+		view.render();
+		model.update();
 	}
-	SDL_DestroyRenderer(rend);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
 }
